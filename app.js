42e9de8d99cd2439290b978067d8de85116a20e6
tegan hansen <TEGAN.HANSEN9352@stu.mtec.edu>
2022-10-13 22:36:46 -0600

    files added

---
+++ app.js
@@ -0,0 +1,11 @@
+$('.navItem').each(function(){
+    $(this).css('color', 'white');
+});
+
+$('#nav').css('background-color', '#145');
+
+$('li:even').css('background-color', '#ccc');
+
+$('li:odd').css('background-color', '#aaa');
+
+$(':input').val('tegan');
